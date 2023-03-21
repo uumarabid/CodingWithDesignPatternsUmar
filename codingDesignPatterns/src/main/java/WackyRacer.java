542bfcf727552c10aa3af9f6dac40a796eb7baeb
Umar1311829 <star_umar2008@hotmail.com>
2023-03-21 12:16:05 +0000

    Create class with instance variables

---
+++ codingDesignPatterns/src/main/java/WackyRacer.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author star_
+ */
+public class WackyRacer {
+    String contestantName;
+    String carName;
+    int carNumber;
+    int numberOfContestants;
+    double height;
+    int speed;
+    int distance;
+
+   
+    
+    
+    
+    
+}
