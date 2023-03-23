1c87f55834d970e3c9344ada08d985dc05bf9fb8
Umar1311829 <star_umar2008@hotmail.com>
2023-03-23 12:18:06 +0000

    feature!: Add Race class, ArrayList, register, remove observer method

---
+++ codingDesignPatterns/src/main/java/Race.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author star_
+ */
+public class Race {
+    
+    private List<RaceObserver> observers = new ArrayList<>();
+    
+    public void registerObserver(RaceObserver observer){
+        observers.add(observer);
+    }
+    
+}
