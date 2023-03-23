3abb592a465b413a7be20db07dcdbbee59f22f0a
Umar1311829 <star_umar2008@hotmail.com>
2023-03-23 12:19:08 +0000

    fix!: code formate

--- codingDesignPatterns/src/main/java/Race.java
+++ codingDesignPatterns/src/main/java/Race.java
@@ -6,17 +6,20 @@ import java.util.List;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author star_
  */
 public class Race {
-    
+
     private List<RaceObserver> observers = new ArrayList<>();
-    
-    public void registerObserver(RaceObserver observer){
+
+    public void registerObserver(RaceObserver observer) {
         observers.add(observer);
     }
-    
+
+    public void removeObserver(RaceObserver observer) {
+        observers.remove(observer);
+    }
+
 }
