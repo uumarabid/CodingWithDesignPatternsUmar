82175158eab213e9629c977fc0055a147fdaa9a7
Umar1311829 <star_umar2008@hotmail.com>
2023-03-23 13:04:54 +0000

    fix!: code clean-up

--- codingDesignPatterns/src/main/java/DetailsRaceObserver.java
+++ codingDesignPatterns/src/main/java/DetailsRaceObserver.java
@@ -8,14 +8,14 @@
  * @author star_
  */
 public class DetailsRaceObserver implements RaceObserver {
-    
+
     public void update(WackyRacer racer) {
-    
-        if(racer.getDistance()>=100){
-            System.out.println(racer.getContestantName()+ 
-                    " has completed the race and travelled  " +
-                    racer.getDistance()+ " miles");
+
+        if (racer.getDistance() >= 100) {
+            System.out.println(racer.getContestantName()
+                    + " has completed the race and travelled  "
+                    + racer.getDistance() + " miles");
         }
     }
-    
+
 }
