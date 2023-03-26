58afcbf304d65db2e2d239afdb5c4a18ddfabe94
Umar1311829 <star_umar2008@hotmail.com>
2023-03-26 14:26:53 +0100

    feature!: Create class AggressiveDriveStrategy and implements interface (DriveStrategy)

---
+++ codingDesignPatterns/src/main/java/AggressiveDriveStrategy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author star_
+ */
+public class AggressiveDriveStrategy implements DriveStrategy{
+
+public void drive(){
+    System.out.println("Driving aggressively to overtake other cars.");
+}
+    
+}
