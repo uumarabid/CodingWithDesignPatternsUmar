25e035707fda725a1d32b6e304ca1ca8b0506d7a
Umar1311829 <star_umar2008@hotmail.com>
2023-03-26 14:19:20 +0100

    feature!: Create a Car class

---
+++ codingDesignPatterns/src/main/java/Car.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author star_
+ */
+public class Car {
+    DriveStrategy driveStrategy;
+
+    public Car(DriveStrategy driveStrategy) {
+        this.driveStrategy = driveStrategy;
+    }
+    
+    
+    
+}
