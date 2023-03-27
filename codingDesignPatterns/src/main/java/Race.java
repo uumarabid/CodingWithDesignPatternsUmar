52e9ab996a4df64cf793a58fbac0e8dc1082a8b2
Umar1311829 <star_umar2008@hotmail.com>
2023-03-27 11:08:17 +0100

    feature!:  initialise new array lists

--- codingDesignPatterns/src/main/java/Race.java
+++ codingDesignPatterns/src/main/java/Race.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,10 +13,14 @@
  */
 public class Race {
 
-    int laps;
+    private int laps;
+    private List <Car> cars;
+    private List<RaceObserver> observers = new ArrayList<>();
 
     public Race(int laps) {
         this.laps = laps;
+        this.cars = new ArrayList<>();
+        
     }
 
 }
