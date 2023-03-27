a9896e84a34b90c4b2c3b75591733f8f6df55e12
Umar1311829 <star_umar2008@hotmail.com>
2023-03-27 12:28:13 +0100

    feature!: Add some methods

--- codingDesignPatterns/src/main/java/Race.java
+++ codingDesignPatterns/src/main/java/Race.java
@@ -6,7 +6,6 @@ import java.util.List;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author star_
@@ -14,13 +13,29 @@ import java.util.List;
 public class Race {
 
     private int laps;
-    private List <Car> cars;
+    private List<Car> cars;
     private List<RaceObserver> observers = new ArrayList<>();
 
     public Race(int laps) {
         this.laps = laps;
         this.cars = new ArrayList<>();
-        
+
+    }
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public void startRace() {
+        System.out.println("Race started");
+    }
+
+    public void endRace() {
+        System.out.println("Race ended");
+    }
+
+    public boolean isOver() {
+        return false;
     }
 
 }
