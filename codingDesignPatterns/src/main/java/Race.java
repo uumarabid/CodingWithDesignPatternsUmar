0946b947ccb88df80a87c412dcf1c47c005eea8d
Umar1311829 <star_umar2008@hotmail.com>
2023-03-27 10:51:31 +0100

    feature!: Add Race class, instance variable and constructor

---
+++ codingDesignPatterns/src/main/java/Race.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author star_
+ */
+public class Race {
+
+    int laps;
+
+    public Race(int laps) {
+        this.laps = laps;
+    }
+
+}
