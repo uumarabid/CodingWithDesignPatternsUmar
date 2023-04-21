e0cb6981b842d7881af48d7e890ccd165bc88c0d
Umar1311829 <star_umar2008@hotmail.com>
2023-04-21 17:06:15 +0100

    Refactor Car class

--- codingDesignPatterns/src/main/java/Car.java
+++ codingDesignPatterns/src/main/java/Car.java
@@ -13,67 +13,33 @@ import java.util.Random;
  */
 public class Car {
 
-    IDriveStrategy driveStrategy;
-    int speed;
-    int distance;
-    LocalTime startTime;    
-    LocalTime finishTime;
+    private String name;
+    private int speed;
+    private int distance;
 
-            
-//    String winner = null;
-    String name;
-//    private int lapsCompleted;
-
-    public Car(IDriveStrategy driveStrategy, String name) {
-        this.driveStrategy = driveStrategy;
-        this.speed = new Random().nextInt(20);
-        this.distance = 0;
-//        this.lapsCompleted = 0;
+    public Car(String name, int speed) {
         this.name = name;
-        this.startTime = LocalTime.now();
-    }
-
-    public void setDriveStrategy(IDriveStrategy driveStrategy) {
-        this.driveStrategy = driveStrategy;
+        this.speed = speed;
+        this.distance = 0;
     }
 
     public String getName() {
         return name;
-    }
-//
-//    public void completeLap() {
-//        lapsCompleted++;
-//    }
-//
-//    public int getLapsCompleted() {
-//        return lapsCompleted;
-//    }
-
-    public void move() {
-        driveStrategy.drive();
     }
 
     public int getSpeed() {
         return speed;
     }
 
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    
-    public void setFinishTime(LocalTime time) {
-        this.finishTime = time;
-    }
-    
     public int getDistance() {
-        return this.speed * Duration.between(startTime, LocalTime.now()).getNano();
+        return distance;
     }
 
-    public void getDescription() {
-        System.out.println("\nname: " + this.name 
-                           + "\nSpeed: " + speed
-                           + "\nDistance: " + (getDistance()/1000000) + " miles"
-                            + "\n=======\n");
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public void move() {
+        distance += speed;
     }
 }
