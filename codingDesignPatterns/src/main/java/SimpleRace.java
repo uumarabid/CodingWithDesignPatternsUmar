19e91e70421b97c5d1a2d491a74f5ccf5aede6ea
Umar1311829 <star_umar2008@hotmail.com>
2023-04-21 18:45:08 +0100

    feat: Implements RaceStrategy intreface

--- codingDesignPatterns/src/main/java/SimpleRace.java
+++ codingDesignPatterns/src/main/java/SimpleRace.java
@@ -5,16 +5,16 @@ import java.util.Random;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author star_
  */
-public class SimpleRace {
+public class SimpleRace implements RaceStrategy{
+
     private int numCircuits;
     private int circuitLength;
     private int winnerIndex;
-    private int[] circuitTimes; 
+    private int[] circuitTimes;
 
     public SimpleRace(int numCircuits, int circuitLength) {
         this.numCircuits = numCircuits;
@@ -22,7 +22,11 @@ public class SimpleRace {
         this.circuitTimes = new int[2];
         Random random = new Random();
     }
-    
-    
-    
+
+    public void race(Car car, RaceObserver observer) {
+        int circuit = 0;
+    // https://www.tutorialspoint.com/java/lang/system_currenttimemillis.htm#:~:text=currentTimeMillis()%20method%20returns%20the,units%20of%20tens%20of%20milliseconds.
+    long startTime = System.currentTimeMillis();
+    }
+
 }
