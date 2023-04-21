22949d00ed8f9168b4d5dbb39e69970f66cbdfa1
Umar1311829 <star_umar2008@hotmail.com>
2023-04-21 18:36:28 +0100

    feat:  Constructor function with two parameters & initialises the instance variables

--- codingDesignPatterns/src/main/java/SimpleRace.java
+++ codingDesignPatterns/src/main/java/SimpleRace.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -12,6 +15,13 @@ public class SimpleRace {
     private int circuitLength;
     private int winnerIndex;
     private int[] circuitTimes; 
+
+    public SimpleRace(int numCircuits, int circuitLength) {
+        this.numCircuits = numCircuits;
+        this.circuitLength = circuitLength;
+        this.circuitTimes = new int[2];
+        Random random = new Random();
+    }
     
     
     
