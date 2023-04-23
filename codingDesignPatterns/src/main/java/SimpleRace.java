c4e80a10ba612fb21d8ad11d3d6df41d9d85dadd
Umar1311829 <star_umar2008@hotmail.com>
2023-04-23 20:31:06 +0100

    feat: Add SimpleRace class and implements RaceStrategy

--- codingDesignPatterns/src/main/java/SimpleRace.java
+++ codingDesignPatterns/src/main/java/SimpleRace.java
@@ -9,7 +9,7 @@ import java.util.Random;
  *
  * @author star_
  */
-public class SimpleRace implements RaceStrategy{
+public class SimpleRace implements RaceStrategy {
 
     private int numCircuits;
     private int circuitLength;
@@ -25,8 +25,46 @@ public class SimpleRace implements RaceStrategy{
 
     public void race(Car car, RaceObserver observer) {
         int circuit = 0;
-    // https://www.tutorialspoint.com/java/lang/system_currenttimemillis.htm#:~:text=currentTimeMillis()%20method%20returns%20the,units%20of%20tens%20of%20milliseconds.
-    long startTime = System.currentTimeMillis();
+        // https://www.tutorialspoint.com/java/lang/system_currenttimemillis.htm#:~:text=currentTimeMillis()%20method%20returns%20the,units%20of%20tens%20of%20milliseconds.
+        long startTime = System.currentTimeMillis();
+
+        // iterate untill all circuits are completed
+        while (circuit < numCircuits) {
+            // call the move method on the car object
+            car.move();
+
+            // check conditions
+            if (car.getDistance() >= circuitLength) {
+                car.setDistance(car.getDistance() - circuitLength);
+                circuit++;
+
+                long currentTime = System.currentTimeMillis();
+                int circuitTime = (int) (currentTime - startTime);
+                circuitTimes[winnerIndex] += circuitTime;
+
+                // notify observer 
+                observer.onCircuitCompleted(car, circuitTime);
+
+                if (winnerIndex == 0 && circuit == numCircuits) {
+                    break;
+                } else if (winnerIndex == 1 && circuit == numCircuits - 1) {
+                    break;
+                }
+
+            }
+            // https://www.educative.io/answers/what-is-the-printstacktrace-method-in-java
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    // returns the array of circuit times
+    public int[] getCircuitTimes() {
+        return circuitTimes;
     }
 
 }
